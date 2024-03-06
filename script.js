8de7040fb25e22be14d7de3a939d6ab34629bfd4
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-03-06 03:04:23 +0000

    Add some basic structure

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+const addBook = document.getElementById(addBook);
+const library = document.querySelector('.library');
+
+function generateBook() {
+    //constructor
+}
+
+function addToLibrary() {
+    //Add new object to array
+    //Call draw function on updated array
+}
+
