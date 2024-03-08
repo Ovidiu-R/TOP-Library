f0b55b310efccb3db6099dea2e79bff6c35648b8
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-03-08 20:43:31 +0000

    Create constructor and addToLibrary function

--- script.js
+++ script.js
@@ -9,20 +9,40 @@ const library = document.querySelector('.library');
 
 openModal.addEventListener('click', () => {
     dialog.showModal();
-    const addBook = document.getElementById('addBook').addEventListener('click', () => {
-        //if fields are all filled in
-        generateBook(/*various attributes necessary*/);
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+    const readStatus = document.getElementById('readStatus');
+    document.getElementById('addBook').addEventListener('click', () => { 
+        if (title.validity.valid && author.validity.valid) {
+        let newBook = new generateBook(title.value, author.value, pages.value, readStatus.checked);
+        addToLibrary(newBook);
+        resetModal();
         dialog.close();
+        }
     });
 });
 
-function generateBook(/*parameters*/) {
-    //constructor
+function generateBook(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+    this.index = myLibrary.length;
 }
 
-function addToLibrary() {
-    //Add new object to array
-    //Call draw function on updated array
+function addToLibrary(newBook) {
+    myLibrary.push(newBook);
+    console.log(myLibrary);
 }
 
 });
+
+function resetModal() {
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    readStatus.checked = false;
+}
+
+/*Function to change object content based on result of read checkbox on each book*/
